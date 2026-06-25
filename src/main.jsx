d50d2488130cd23cb59b7f3816cfc9b554206438
PooyanYagh <95357463+PooyanYagh@users.noreply.github.com>
2026-06-25 18:05:24 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// اطمینان از اعمال فونت
+document.documentElement.style.fontFamily = "'Dana-FaNum', 'Vazirmatn', 'Tahoma', sans-serif";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
